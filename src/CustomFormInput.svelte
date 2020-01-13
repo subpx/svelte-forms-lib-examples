55abfae414ad5f4356714529a500ca232883f2c4
Tom Hynard <tomhynard@gmai.com>
2020-01-13 13:31:48 +1100

    Adding custom form input example

---
+++ src/CustomFormInput.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  const options = ['Male', 'Female'];
+  let selected;
+
+  $: {
+    dispatch('change', selected);
+  }
+
+  function select(value) {
+    selected = value;
+  }
+</script>
+
+<style>
+  .customInput {
+    display: flex;
+  }
+
+  .customInput > div {
+    border: 1px solid black;
+    cursor: pointer;
+    padding: 5px;
+    margin: 0 2px;
+  }
+
+  .selected {
+    background: black;
+    color: white;
+  }
+</style>
+
+<div class="customInput">
+{ #each options as option }
+  <div class:selected="{ selected === option }" on:click="{ () => select(option) }">{ option }</div>
+{ /each }
+</div>
--- src/examples/yup-validation/YupValidation.svelte
+++ src/examples/yup-validation/YupValidation.svelte
@@ -1,23 +1,35 @@
 <script>
   import createForm from "svelte-forms-lib";
   import yup from "yup";
+  import CustomInput from "../../CustomFormInput.svelte";
 
   const { form, errors, state, handleChange, handleSubmit } = createForm({
     initialValues: {
       name: "",
-      email: ""
+      email: "",
+      sex: ""
     },
     validationSchema: yup.object().shape({
       name: yup.string().required(),
       email: yup
         .string()
         .email()
-        .required()
+        .required(),
+      sex: yup.string().required()
     }),
     onSubmit: ({ values }) => {
       console.log("make form request:", values);
     }
   });
+
+  function customInputEvent(name, value) {
+    return {
+      target: {
+        name, 
+        value
+      }
+    }
+  }
 </script>
 
 <style>
@@ -49,6 +61,15 @@
     bind:value={$form.email} />
   {#if $errors.email}
     <hint class="error">{$errors.email}</hint>
+  {/if}
+
+  <label for="sex">sex</label>
+  <CustomInput 
+    on:change={(event) => handleChange(customInputEvent('sex', event.detail))}
+    bind:selected={$form.sex}
+  />
+  {#if $errors.sex}
+    <hint class="error">{$errors.sex}</hint>
   {/if}
 
   <button type="submit">submit</button>
